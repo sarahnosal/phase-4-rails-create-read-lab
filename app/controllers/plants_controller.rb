f1336d362196cf8f91b7f8791fb930d80a109923
Sarah Nosal <nosalsm@gmail.com>
2022-01-31 21:11:47 -0600

    completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+
+    def index
+        render json: Plant.all
+    end
+
+    def show
+        render json: Plant.find(params[:id])
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
